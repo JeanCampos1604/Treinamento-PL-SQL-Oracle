b2c23842737f2bf0e8ceeb61de1eb77578a3d8f6
JeanCampos1604 <jeanflaviodecampos1604@gmail.com>
2023-02-21 11:23:38 -0300

    Update 21/02/2023 - BI e Exercícios BD - by HinoDesktop

---
+++ Material/Treinamento Hino/Will/Ex4/Enunciado.sql
@@ -0,0 +1,22 @@
+TABELAS:
+        FSNFSAIDA
+        FSNFITEM
+        FSNFITEMIMP
+        GEEMPRESA
+        FSIMPOSTOS
+        GEENDEMPRESA
+        GEENDERECO
+
+COLUNAS:
+        Número da Nota,
+        Série da Nota,
+        Modelo da Nota,
+        Data de Emissão,
+        Cod. do Item,
+        Descrição do Item,
+        Impostos (separados),
+        Total de impostos,
+        Valor do item (sem imposotos),
+        Valor do item com impostos,
+        Nome da empresa,
+        Estado do endereço comercial da empresa.
